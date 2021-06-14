fd2a7654cdab8ab8d90f21b73fdd5109339538ec
kfield1 <kayt1303@yahoo.com>
2021-06-14 17:18:41 -0500

    adding

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello World"
+
+@app.route("/json")
+def json_route():
+    j = [
+        {1:12},
+        {2:13},
+        {'something', 'else'},
+    ]
+    return jsonify(j)
+
     
+if __name__ =="__main__":
+    app.run(debug=True)
