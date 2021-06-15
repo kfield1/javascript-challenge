a5eb39b16e99605a5f252cfc0d7957a8f7d3a68f
kfield1 <kayt1303@yahoo.com>
2021-06-14 21:16:56 -0500

    fixed table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,46 +1,26 @@
 // from data.js
 var tableData = data;
 
-
 // Creating References to tbody, input and button
 var $tbody = d3.select("tbody");
-
-
 var button = d3.select("#filter-btn");
-
-
 var inputFieldDate = d3.select("#datetime");
-
-
 var inputFieldCity = d3.select("#city");
-
-
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 // Inputing the data into the HTML
-
-
 var addData = (dataInput) => {
-
-
-   dataInput.forEach(ufoSightings => {
-
-
-       var row = $tbody.append("tr");
-
-
-       columns.forEach(column => row.append("td").text(ufoSightings[column])
-       )
-
-
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
    });
-
-
 }
+
+addData(tableData)
+
 // Event Handler Function
 function runEvent(){
-
-
    // Prevent Page from Refreshing
    //d3.event.preventDefault();
    // Select the input element and get the raw HTML node
