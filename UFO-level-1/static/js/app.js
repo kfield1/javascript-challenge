7013a3ff7516a040151a44464a47b4b52b22520b
kfield1 <kayt1303@yahoo.com>
2021-06-14 20:05:54 -0500

    more work

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,67 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+// Creating References to tbody, input and button
+var $tbody = d3.select("tbody");
+
+
+var button = d3.select("#filter-btn");
+
+
+var inputFieldDate = d3.select("#datetime");
+
+
+var inputFieldCity = d3.select("#city");
+
+
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+// Inputing the data into the HTML
+
+
+var addData = (dataInput) => {
+
+
+   dataInput.forEach(ufoSightings => {
+
+
+       var row = $tbody.append("tr");
+
+
+       columns.forEach(column => row.append("td").text(ufoSightings[column])
+       )
+
+
+   });
+
+
+}
+// Event Handler Function
+function runEvent(){
+
+
+   // Prevent Page from Refreshing
+   //d3.event.preventDefault();
+   // Select the input element and get the raw HTML node
+
+   var inputElement = d3.select("#datetime");
+
+   // Get the input value (date) from the input element
+   var inputData = inputElement.property("value");
+
+   //Check for input data
+       if (inputData == "") {inputData = tableData[0].datetime;}
+
+
+   // Filter the data.js by the input value
+   var filteredData = tableData.filter(sighting => sighting.datetime === inputData);
+
+   // Call the createTable function with the
+   // filteredData as the parameter
+
+   createTable(filteredData);
+
+};
+
+runEvent();
